f71cf3009659ebc51ce2968daa53c4fc92763ee1
ZXHSunSalt <33613991+ZXHSunSalt@users.noreply.github.com>
2019-04-12 15:07:01 +0800

    params in ActiveMQ

---
+++ ActiveMQ/Configuration.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2019年4月12日
+
+@author: Administrator
+'''
+configs ={
+    "path":'E:\\Listeners_data\\',
+    "caffe_root" :'D:\\caffe\\caffe-master',
+    "model_prototxt_path":'D:/EclipseWorkspace/caffe_exe/deploy_full_conv.prototxt',
+    "caffe_model_path":'D:/EclipseWorkspace/caffe_exe/model_60000_conv.caffemodel'
+}
+
+        
